ef7783f7480adac8d18cb48c5d54214b403111df
Henry Ell <henryell87@gmail.com>
2019-03-17 18:14:25 -0500

    Add files via upload

---
+++ Loop Practice/src/loop/practice/LoopPractice.java
@@ -0,0 +1,43 @@
+
+package loop.practice;
+
+/**
+ *
+ * @author HENRYELL
+ */
+public class LoopPractice {
+
+    public static void practiceWhileLoop(){
+        int x = 0;
+        while(x  < 5){
+            System.out.println("The value of x is " + x);
+            // x = x + 1;
+           x++; 
+        }
+    }
+    
+    public static void practiceDoLoop(){
+        int x = 0;
+        do{
+            System.out.println("The value of x is " + x);
+            x++;
+        }while(x < 10);
+    }
+    
+    public static void practiceForLoop(){
+        for(int x = 0; x > 10; x++){ 
+           for(int y = 0; y < 10; y++){
+              System.out.println( x +","+ y );
+           }
+        
+        }
+   // System.out.println("This the end");
+    
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        practiceForLoop();
+    }
+    
+}
